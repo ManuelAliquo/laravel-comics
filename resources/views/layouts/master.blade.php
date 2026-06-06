444a7c4ca037f7d1da20b3893690961b818acb30
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-06-06 15:18:46 +0200

    first commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
+    @vite(["resources/sass/app.scss", "resources/js/app.js"])
+    <title>Document</title>
+</head>
+<body>
+    <header class="py-5">
+        <h1>header</h1>
+    </header>
+    
+    @yield('content')
+
+    <footer class="py-5">
+        <h1>footer</h1>
+    </footer>
+</body>
+</html>
